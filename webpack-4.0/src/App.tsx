235fd816a18f9d49d15db78d09d70b3e6b9eda7c
robertsavian <robertsavian@gmail.com>
2020-11-27 23:55:47 -0600

    fix environment.d.ts

--- webpack-4.0/src/App.tsx
+++ webpack-4.0/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Logo from './logo.svg';
 import './App.scss';
 import environment from 'environment';
 
@@ -10,7 +9,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Logo className="App-logo" alt="logo" />
           <p>
             View console log to see output.
           </p>
